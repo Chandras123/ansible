6e9ee9bf1f92ffbcf2854d591ee1b556c091fb8f
chandra <chandraannem01@gmail.com>
2025-05-30 17:13:45 +0530

    hello

--- in.sh
+++ in.sh
@@ -9,16 +9,16 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 # Common variables
 AMI_ID="ami-09c813fb71547fc4f"         # Replace with your AMI ID
 INSTANCE_TYPE="t2.micro"                      # Replace with your key pair name
-SECURITY_GROUP="sgr-0361f980111982118"   # Replace with your security group ID
+SECURITY_GROUP="sg-02e1cec7f1f2816eb"   # Replace with your security group ID
 
-for NAME in "${INSTANCES[@]}"; do
-  echo "Launching instance: $NAME"
+for INSTANCE in "${INSTANCES[@]}"; do
+  echo "Launching instance: $INSTANCE"
   aws ec2 run-instances \
     --image-id $AMI_ID \
     --count 1 \
     --instance-type $INSTANCE_TYPE \
     --security-group-ids $SECURITY_GROUP \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$NAME}]"
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]"
 done
 
 echo "All instances creation initiated."
