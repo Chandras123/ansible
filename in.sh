0b1e952a8f0b577ef96fcabb9a7d727043392580
chandra <chandraannem01@gmail.com>
2025-05-30 17:50:59 +0530

    hello

--- in.sh
+++ in.sh
@@ -13,9 +13,10 @@ SECURITY_GROUP="sg-02e1cec7f1f2816eb"  # Replace with your security group ID
 region="us-east-1"  # Replace with your desired region
 aws configure set region $region
 INSTANCES=("mongodb" "catalogue" "user" "cart" "shipping" "frontend" "payment" "rabbitmq")
-# Loop through the instance names and create each instance
-for instance_name in "${INSTANCES[@]}"; do
-  echo "Creating instance: $instance_name"
-  aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type $INSTANCE_TYPE --key-name roboshop --security-group-ids $SECURITY_GROUP --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance_name}]" --region $region
+# writing code for creating all instances present in the list
+# Create instances using the AWS CLI
+#looping through the instances and creating them
+for instance in "${INSTANCES[@]}"; do
+  echo "Creating instance: $instance"
+  aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type $INSTANCE_TYPE --key-name roboshop --security-group-ids $SECURITY_GROUP --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --region $region
 done
-echo "All instances created successfully."
