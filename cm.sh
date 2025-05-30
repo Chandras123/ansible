739a9b2d518294ef69cfcf956590404ea646821e
chandra <chandraannem01@gmail.com>
2025-05-30 18:02:58 +0530

    hello

---
+++ cm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#writng a script to create a list of EC2 instances using AWS CLI
+# List of instance names to create
+# Common variables
+AMI_ID="ami-09c813fb71547fc4f"         # Replace with your AMI ID
+INSTANCE_TYPE="t2.micro"                      # Replace with your key pair name
+SECURITY_GROUP="sg-02e1cec7f1f2816eb"  # Replace with your security group ID
+region="us-east-1"  # Replace with your desired region
+aws configure set region $region
+
+INSTANCE_NAMES=( $@) # Accepting instance names as command line arguments
+# writing code for creating all instances present in the list
+KEY_NAME="my-key"                      # Replace with your key pair name
+SECURITY_GROUP="sg-0123456789abcdef"   # Replace with your security group ID
+
+for NAME in "${INSTANCE_NAMES[@]}"; do
+  echo "Launching instance: $NAME"
+  aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --count 1 \
+    --instance-type $INSTANCE_TYPE \
+    --security-group-ids $SECURITY_GROUP \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$NAME}]"
+done
+
+# Wait for instances to be in running state
+aws ec2 wait instance-running --filters "Name=tag:Name,Values=${INSTANCE_NAMES[*]}"
+echo "Instances launched and are now running."
