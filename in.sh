5761a1c6515401316b4ac50e501ee6c5eae94ff2
chandra <chandraannem01@gmail.com>
2025-05-30 17:37:01 +0530

    hello

--- in.sh
+++ in.sh
@@ -3,14 +3,16 @@
 # Script to create a list of EC2 instances using AWS CLI
 
 # List of instance names to create
-INSTANCES=("mongodb")
+
 
 
 # Common variables
 AMI_ID="ami-09c813fb71547fc4f"         # Replace with your AMI ID
 INSTANCE_TYPE="t2.micro"                      # Replace with your key pair name
-SECURITY_GROUP="sg-02e1cec7f1f2816eb"   # Replace with your security group ID
-
+SECURITY_GROUP="sg-02e1cec7f1f2816eb"  # Replace with your security group ID
+region="us-east-1"  # Replace with your desired region
+aws configure set region $region
+INSTANCES=("mongodb" "catalogue" "user" "cart" "shipping" "frontend" "payment" "rabbitmq")
 for INSTANCE in "${INSTANCES[@]}"; do
   echo "Launching instance: $INSTANCE"
   aws ec2 run-instances \
